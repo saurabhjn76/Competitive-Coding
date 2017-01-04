3cedce96d35ce557ae3c53d9c379731b583a5190
saurabhjn76 <saurabhjn76@gmail.com>
2017-01-04 11:11:08 +0530

    solved todays 5 of 5 easy.codechef

--- CodeChef/Easy/AMSGAME1.cpp
+++ CodeChef/Easy/AMSGAME1.cpp
@@ -14,23 +14,44 @@ using namespace std;
 
 typedef long long ll;
 
+ ll gcd( ll a, ll b)
+{
+     ll rem,max,min;
+
+ //printf("Enter the TWo no. for HCF calculation \n");
+ // scanf("%d%d",&a,&b);
+ if(a>=b){   
+  max=a;
+  min=b;
+  }
+ else{
+  max=b;
+  min=a;
+  }
+ while(min!=0){
+    rem=max%min;
+    max=min;
+    min=rem;
+    }
+ // printf("The hcf of given number %d & %d is :%d\n",a,b,max);
+ return max;
+ }
 int main()
 {
   int t;
   cin >> t;
   while(t--){
-  int n;
-  s(n);
-  int a[n+n/25+1];
-  for(int i=0;i<n + (n-1)/25 +1;i++){
-  	a[i]=i%26;
-  }
-  for(int i=n+(n-1)/25;i>=0;i--){
-  	printf("%c",97+a[i]);
-  }
-  printf("\n");
+   int n; 
+   s(n); ll a[n];
+   for(int i=0;i<n;i++)
+   	sl(a[i]);
+   ll gcdd = a[0];
+   for(int i=1;i<n;i++){
+   	gcdd=gcd(a[i],gcdd);
+   }
+   printf("%lld\n",gcdd);
+
   }
 	
 	return 0;
 }
-	
