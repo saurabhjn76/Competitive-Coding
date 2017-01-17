57cd930d79a89f73319c737cc55c4da8fc90f47c
saurabhjn76 <saurabhjn76@gmail.com>
2017-01-17 12:56:00 +0530

    Added solution

---
+++ CodeChef/Easy/TOTR.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+# define s(n)                        scanf("%d",&n)
+# define sc(n)                       scanf("%c",&n)
+# define sl(n)                       scanf("%lld",&n)
+# define sf(n)                       scanf("%lf",&n)
+# define ss(n)                       scanf("%s",n)
+
+# define INF                         (int)1e9
+# define EPS                         1e-9
+# define MOD 1000000007
+
+
+typedef long long ll;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--){
+  
+  }
+	
+	return 0;
+}
+	
