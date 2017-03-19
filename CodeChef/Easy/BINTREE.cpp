ca145ac449f229b43242465f69732a4228d5f75b
saurabhjn76 <saurabhjn76@gmail.com>
2017-03-19 23:56:12 +0530

    added codes

--- CodeChef/Easy/BINTREE.cpp
+++ CodeChef/Easy/BINTREE.cpp
@@ -1,1 +1,34 @@
-BINTREE.cpp
+#include "bits/stdc++.h"
+using namespace std;
+
+# define s(n)                        scanf("%d",&n)
+# define sc(n)                       scanf("%c",&n)
+# define sl(n)                       scanf("%lld",&n)
+# define sf(n)                       scanf("%lf",&n)
+# define ss(n)                       scanf("%s",n)
+
+#define R(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+
+# define INF                         (int)1e9
+# define EPS                         1e-9
+# define MOD 1000000007
+
+
+typedef long long ll;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--){
+   ll i,j;
+   sl(i);sl(j);
+   
+   
+  }
+	
+	return 0;
+}
+	
